8640bb246629fce9998103bab0dab902600cbe5b
Jelena <jelenarankovic0@gmail.com>
2022-10-23 22:26:27 +0200

    using templates refs

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     <button @click="setAge">Change Age</button>
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
-      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
@@ -17,6 +18,7 @@ export default {
     /* const uName = ref('Jelena');*/
     const firstName = ref('');
     const lastName = ref('');
+    const lastNameInput = ref('null');
     const uAge = ref(36);
 
     // const user = reactive({
@@ -39,12 +41,17 @@ export default {
       uAge.value = 37;
     }
 
+    function setLastName() {
+      lastName.value = lastNameInput.value.value;
+    }
+
     return {
       userName: uName,
       age: uAge,
       setAge: setNewAge,
       firstName,
-      lastName,
+      lastNameInput,
+      setLastName,
     };
   },
 
